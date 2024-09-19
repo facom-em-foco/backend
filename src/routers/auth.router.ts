64099671c08afb3ef22b335f601074e476b67fcb
Eduardo Santos Luz <edyven100@gmail.com>
2024-09-18 21:13:46 -0400

    style: Rename controllers

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import UserController from '@/controllers/user.controlller';
+import UserController from '@/controllers/user.controller';
 
 const authController = new UserController();
 const authRouter = Router();
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,6 +1,5 @@
-import { Router } from "express";
-import UserController from "@/controllers/user.controlller";
-
+import { Router } from 'express';
+import UserController from '@/controllers/user.controller';
 
 const userController = new UserController();
 const userRouter = Router();
