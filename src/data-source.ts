eb0e9145ead2c51f5e92a2cb7e0de09a53acba1c
Joao Pedro Figueiredo de Oliveira <joao_pedro_oliveira@ufms.br>
2024-09-24 18:58:12 -0400

    feat:initial data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,15 @@
-module.exports = {
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
+  port: parseInt(process.env.DATABASE_PORT || "5432", 10),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   entities: ["src/entities/*.ts"],
   migrations: ["src/migration/*.ts"],
-  cli: {
-    migrationsDir: "src/migration"
-  }
-};
+  synchronize: false,
+});
