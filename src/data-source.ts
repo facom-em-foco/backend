e9e35398d27c13295faa88ac3878c0f8b96c79ca
Eduardo Santos Luz <edyven100@gmail.com>
2024-09-24 21:30:27 -0400

    feat: Migrations generate and run scripts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,11 @@
-import { DataSource } from "typeorm";
-import * as dotenv from "dotenv";
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import config from './ormconfig';
+
 dotenv.config();
 
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT || "5432", 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migration/*.ts"],
-  synchronize: false,
-});
+const appDataSourceConfig: any = {
+  ...config,
+};
+
+export const AppDataSource = new DataSource(appDataSourceConfig);
